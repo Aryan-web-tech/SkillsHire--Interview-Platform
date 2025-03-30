48ccd8dea89c3d62ddad0a207a3a5f1e7ad8a90f
Aryan-web-tech <aryanapj124@gmail.com>
2025-03-30 14:20:47 +0530

    Initial commit

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://climbing-cattle-3.clerk.accounts.dev/",
+      domain: "https://proud-squirrel-97.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
